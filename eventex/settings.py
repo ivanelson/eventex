d36be570196cee0cecc1d4185c9bce48210ce063
Ivan Nunes <iv4n.nunes@gmail.com>
2018-06-11 16:54:19 -0300

    reenvio

--- eventex/settings.py
+++ eventex/settings.py
@@ -22,11 +22,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = '1e6e^fojets2i7g93bo9n4lvgug70jy!4hy%d(xci=zsg-mn%g'
+# SECRET_KEY = '1e6e^fojets2i7g93bo9n4lvgug70jy!4hy%d(xci=zsg-mn%g'
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = True
+# DEBUG = True
 DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = ['*']
@@ -120,4 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT =
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
